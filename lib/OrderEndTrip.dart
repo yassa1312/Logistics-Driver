a71b12637fdbd93638ba433b3e56aa70675b1a19
Yassa Ashraf <139929196+yassa1312@users.noreply.github.com>
2024-04-20 00:06:09 +0200

    End Trip

---
+++ lib/OrderEndTrip.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/material.dart';
+import 'package:logistics/auth_service.dart';
+import 'package:logistics/main.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'OrderViewMyDeliveredRequests..dart';
+
+
+
+
+class OrderEndTrip extends StatelessWidget {
+  final Order order;
+
+  const OrderEndTrip({required this.order, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Order Details',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.orange,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildOrderInfo('Request ID:', order.requestId),
+                    _buildOrderInfo('Pick Up Location:', order.pickUpLocation),
+                    _buildOrderInfo('Drop Off Location:', order.dropOffLocation),
+                    _buildOrderInfo('Time Stamp On Creation:', order.timeStampOnCreation),
+                    _buildOrderInfo('Ride Type:', order.rideType),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _StartOrder(context, order);
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                          child: Text(
+                            'Start',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _refuseOrder(context, order);
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                          child: Text(
+                            'Refuse',
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrderInfo(String title, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),
+        ),
+        SizedBox(height: 8),
+        Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: title == 'Delivery Location'
+              ? InkWell(
+            onTap: () {
+              _launchMap(value);
+            },
+            child: Text(
+              value,
+              style: TextStyle(fontSize: 16, color: Colors.blue, decoration: TextDecoration.underline),
+            ),
+          )
+              : Text(
+            value,
+            style: TextStyle(fontSize: 16, color: Colors.black87),
+          ),
+        ),
+        SizedBox(height: 16),
+      ],
+    );
+  }
+
+
+  void _refuseOrder(BuildContext context, Order order) async {
+    // Check if startTripTime is empty
+    if (order.startTripTime.isNotEmpty) {
+      Fluttertoast.showToast(
+        msg: 'Cannot refuse order with start trip time',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return; // Return early if startTripTime is not empty
+    }
+
+    try {
+      String? token = await AuthService.getAccessToken();
+
+      if (token == null) {
+        print('Access token not found.');
+        return;
+      }
+
+      String url = 'http://www.logistics-api.somee.com/api/Driver/RemoveAcceptedRequest/${order.requestId}';
+
+      Map<String, String> headers = {
+        'Authorization': 'Bearer $token',
+        'accept': '*/*',
+      };
+
+      var response = await http.put(
+        Uri.parse(url),
+        headers: headers,
+      );
+
+      if (response.statusCode == 200) {
+        Fluttertoast.showToast(
+          msg: 'Order ${order.requestId} refused',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.green,
+          textColor: Colors.white,
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => Home()),
+        );
+      }  else {
+        // Other server errors
+        print('Failed to start order: ${response.statusCode}');
+        Fluttertoast.showToast(
+          msg: 'Failed to start order ${order.requestId}: ${response.statusCode}',
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+      }
+
+    } catch (error) {
+      print('Error refusing order: $error');
+      Fluttertoast.showToast(
+        msg: 'Error refusing order',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    }
+  }
+
+  void _launchMap(String location) async {
+    String googleUrl = 'https://www.google.com/maps/search/?api=1&query=$location';
+    if (await canLaunch(googleUrl)) {
+      await launch(googleUrl);
+    } else {
+      throw 'Could not launch $googleUrl';
+    }
+  }
+}
+
+Future<void> _StartOrder(BuildContext context, Order order) async {
+  try {
+    String? token = await AuthService.getAccessToken();
+
+    if (token == null) {
+      print('Access token not found.');
+      Fluttertoast.showToast(
+        msg: 'Access token not found.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      return;
+    }
+
+    String url = 'http://www.logistics-api.somee.com/api/Trip/StartTrip/${order.requestId}';
+
+    Map<String, String> headers = {
+      'Authorization': 'Bearer $token',
+      'accept': '*/*',
+    };
+
+    var response = await http.put(
+      Uri.parse(url),
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Fluttertoast.showToast(
+        msg: 'Order ${order.requestId} started successfully',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+    } else if (response.statusCode == 401) {
+      // Unauthorized, token expired or invalid
+      print('Unauthorized: ${response.statusCode}');
+      Fluttertoast.showToast(
+        msg: 'Unauthorized: ${response.statusCode}',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+      // Handle token expiration or invalid token here, maybe redirect to login
+    }  else if (order.startTripTime.isNotEmpty) {
+      // Show a toast message indicating that the trip has already started
+      Fluttertoast.showToast(
+        msg: 'Trip has already started for order ${order.requestId}.',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }else {
+      // Other server errors
+      print('Failed to start order: ${response.statusCode}');
+      Fluttertoast.showToast(
+        msg: 'Failed to start order ${order.requestId}: ${response.statusCode}',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    }
+  } catch (error) {
+    print('Error starting order: $error');
+    Fluttertoast.showToast(
+      msg: 'Error starting order: $error',
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+    );
+  }
+}
---
+++ lib/OrderViewMyDeliveredRequests..dart
@@ -0,0 +1,324 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:logistics/OrderEndTrip.dart';
+import 'package:logistics/OrderRemoveAcceptedRequest.dart';
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+import 'auth_service.dart'; // Make sure this import is correct
+
+class Order {
+  final String requestId;
+  final String pickUpLocation;
+  final String dropOffLocation;
+  final String timeStampOnCreation;
+  final String startTripTime;
+  final String rideType;
+
+  Order({
+    required this.requestId,
+    required this.pickUpLocation,
+    required this.dropOffLocation,
+    required this.timeStampOnCreation,
+    required this.startTripTime,
+    required this.rideType,
+  });
+
+  factory Order.fromJson(Map<String, dynamic> json) {
+    return Order(
+      requestId: json['request_Id'] ?? '',
+      pickUpLocation: json['pick_Up_Location'] ?? '',
+      dropOffLocation: json['drop_Off_Location'] ?? '',
+      timeStampOnCreation: json['time_Stamp_On_Creation'] != null
+          ? DateTime.parse(json['time_Stamp_On_Creation']).toString()
+          : '',
+      startTripTime: json['start_Trip_Time'] != null
+          ? DateTime.parse(json['start_Trip_Time']).toString()
+          : '',
+      rideType: json['ride_Type'] ?? '',
+    );
+  }
+}
+
+class OrderViewMyDeliveredRequests extends StatefulWidget {
+  const OrderViewMyDeliveredRequests({Key? key}) : super(key: key);
+
+  @override
+  _OrderViewMyDeliveredRequestsState createState() => _OrderViewMyDeliveredRequestsState();
+
+
+}
+
+class _OrderViewMyDeliveredRequestsState extends State<OrderViewMyDeliveredRequests> {
+  late List<Order> _orders = [];
+  bool _isLoading = false;
+  String _errorMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOrders(); // Initially fetch orders
+  }
+
+  Future<void> fetchOrders() async {
+    setState(() {
+      _isLoading = true;
+      _errorMessage = '';
+    });
+
+    try {
+      String? token = await AuthService.getAccessToken();
+
+      if (token != null) {
+        var headers = {
+          'Authorization': 'Bearer $token',
+          'accept': '*/*',
+        };
+
+        final url = 'http://www.logistics-api.somee.com//api/Driver/ViewMyAcceptedRequests/1';
+
+        final response = await http.get(
+          Uri.parse(url),
+          headers: headers,
+        );
+
+        print('Response status: ${response.statusCode}');
+        print('Response body: ${response.body}');
+
+        if (response.statusCode == 200) {
+          final List<dynamic> responseData = json.decode(response.body);
+          List<Order> apiOrders = responseData.map((data) => Order.fromJson(data)).toList();
+
+          setState(() {
+            _orders = apiOrders;
+            _isLoading = false;
+          });
+        } else {
+          // Log error if API call fails
+          print('Failed to fetch orders. Status code: ${response.statusCode}');
+          // Fallback to dummy data if API call fails
+          _loadDummyOrders();
+        }
+      } else {
+        // Log error if token is null
+        print('Token is null');
+        // Fallback to dummy data if token is null
+        _loadDummyOrders();
+      }
+    } catch (e) {
+      // Log error if exception occurs
+      print('Exception during API call: $e');
+      // Fallback to dummy data if exception occurs
+      _loadDummyOrders();
+    }
+  }
+
+  void _loadDummyOrders() {
+    // Simulate API call by creating a list of dummy data
+    List<Order> dummyOrders = [
+      Order(
+        requestId: '1',
+        pickUpLocation: 'Location A',
+        dropOffLocation: 'Location B',
+        timeStampOnCreation: '2022-04-10 10:00:00',
+        startTripTime: '2022-04-10 10:00:00',
+        rideType: 'Normal',
+      ),
+      Order(
+        requestId: '2',
+        pickUpLocation: 'Location C',
+        dropOffLocation: 'Location D',
+        timeStampOnCreation: '2022-04-11 12:00:00',
+        startTripTime: '2022-04-10 10:00:00',
+        rideType: 'Premium',
+      ),
+      // Add more dummy orders as needed
+    ];
+
+    // Set the dummy data to the state variable
+    setState(() {
+      _orders = dummyOrders;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'MY Orders',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.orange,
+      ),
+      body: RefreshIndicator(
+        onRefresh: fetchOrders,
+        child: _isLoading
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : _errorMessage.isNotEmpty
+            ? Center(
+          child: Text(_errorMessage),
+        )
+            : _orders.isNotEmpty
+            ? ListView.builder(
+          itemCount: _orders.length,
+          itemBuilder: (context, index) {
+            return OrderTile(
+              order: _orders[index],
+              refreshOrders: fetchOrders, // Pass the fetchOrders function
+            );
+          },
+        )
+            : Center(
+          child: Text('No orders found'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchOrders,
+        tooltip: 'Refresh',
+        child: Icon(Icons.refresh),
+        backgroundColor: Colors.orange,
+      ),
+    );
+  }
+}
+
+class OrderTile extends StatelessWidget {
+  final Order order;
+  final Function refreshOrders; // Define this callback function
+
+  const OrderTile({
+    required this.order,
+    required this.refreshOrders, // Pass this callback through constructor
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _showOrderDetails(context, order),
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildOrderInfo('Request ID:', order.requestId),
+              MapLocationWidget(
+                locationLabel: 'Pick Up Location:',
+                location: order.pickUpLocation,
+              ),
+              MapLocationWidget(
+                locationLabel: 'Drop Off Location:',
+                location: order.dropOffLocation,
+              ),
+              _buildOrderInfo('Time Stamp On Creation:', order.timeStampOnCreation),
+              _buildOrderInfo('Ride Type:', order.rideType),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOrderInfo(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.blue,
+            ),
+          ),
+          SizedBox(height: 4), // Add a SizedBox for spacing
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showOrderDetails(BuildContext context, Order order) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('requestId', order.requestId);
+    prefs.setString('startTripTime', order.startTripTime);
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => OrderEndTrip(order: order)),
+    ).then((result) {
+      if (result == true) {
+        refreshOrders(); // Call the refreshOrders function when needed
+      }
+    });
+  }
+}
+class MapLocationWidget extends StatelessWidget {
+  final String locationLabel;
+  final String location;
+
+  const MapLocationWidget({
+    Key? key,
+    required this.locationLabel,
+    required this.location,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _launchMapUrl(location);
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            '$locationLabel',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+              color: Colors.blue,
+            ),
+          ),
+          SizedBox(height: 4), // Add a SizedBox for spacing
+          Text(
+            '$location',
+            style: TextStyle(
+              fontSize: 16,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _launchMapUrl(String location) async {
+    String googleMapsUrl = 'https://www.google.com/maps/search/?api=1&query=$location';
+    if (await canLaunch(googleMapsUrl)) {
+      await launch(googleMapsUrl);
+    } else {
+      throw 'Could not launch $googleMapsUrl';
+    }
+  }
+}
